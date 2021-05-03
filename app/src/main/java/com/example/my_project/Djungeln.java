aecc9609cf3b578e02f717bdd21d16d7da57a2d2
b20idawa <b20idawa@student.his.se>
2021-05-03 12:20:10 +0200

    Hämtat id på tillbaka knapp i både Djungeln.java och Savannen.java även lagt till en setOnClickListener för knappen som byter aktivitet tillbaka till MainActivity

--- app/src/main/java/com/example/my_project/Djungeln.java
+++ app/src/main/java/com/example/my_project/Djungeln.java
@@ -2,7 +2,11 @@ package com.example.my_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Djungeln extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class Djungeln extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_djungeln);
+
+        Button button = findViewById(R.id.tillbaka2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("knapp", "Du tryckte på tillbaka!");
+                Intent intent = new Intent(Djungeln.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/my_project/Savannen.java
+++ app/src/main/java/com/example/my_project/Savannen.java
@@ -2,7 +2,11 @@ package com.example.my_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Savannen extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class Savannen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savannen);
+
+        Button button = findViewById(R.id.tillbaka1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("knapp", "Du tryckte på tillbaka!");
+                Intent intent = new Intent(Savannen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
