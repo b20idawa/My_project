b08176e48cbb6b16a1f32d6b4e3105888f7495d8
b20idawa <b20idawa@student.his.se>
2021-05-03 11:52:08 +0200

    Lagt till medlemsvariabler i Animals.java

--- app/src/main/java/com/example/my_project/Animals.java
+++ app/src/main/java/com/example/my_project/Animals.java
@@ -1,4 +1,14 @@
 package com.example.my_project;
 
 public class Animals {
+
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int size;
+    private int cost;
+    private String auxdata;
 }
