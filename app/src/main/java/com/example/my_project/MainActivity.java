433cad254c4431f76eecee8c48155800651129e5
b20idawa <b20idawa@student.his.se>
2021-05-03 09:05:20 +0200

    Lagt till clicklistener för varje knapp. Varje knapp har en LogCat för att kontrollera att rätt saker sker vid rätt knapptryckning.

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.my_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = findViewById(R.id.firstbutton);
         Button button2 = findViewById(R.id.secondbutton);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("knapp", "Du tryckte på första knappen!");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("knapp", "Du tryckte på andra knappen!");
+            }
+        });
     }
 }
