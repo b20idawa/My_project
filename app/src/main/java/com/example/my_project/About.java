5e2a8dac508ac23388094b5f4e0fc5d8dc78b2d5
b20idawa <b20idawa@student.his.se>
2021-05-04 10:26:04 +0200

    Tagit bort filer som inte längre används då en aktivitet togs bort i appen. Även bytt namn på en av aktiviteterna för att användningsområdet kommer att ändras.

--- app/src/main/java/com/example/my_project/About.java
+++ app/src/main/java/com/example/my_project/About.java
@@ -2,35 +2,25 @@ package com.example.my_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class Savannen extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_savannen);
+        setContentView(R.layout.activity_about);
 
         Button button = findViewById(R.id.tillbaka1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("knapp", "Du tryckte på tillbaka!");
-                Intent intent = new Intent(Savannen.this, MainActivity.class);
+                Intent intent = new Intent(About.this, MainActivity.class);
                 startActivity(intent);
             }
         });
